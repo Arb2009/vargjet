15a682ac118c5cb0d9869efeea2ba7193b0f48c8
Arb2009 <arbsmajlaj10@gmail.com>
2025-10-01 16:53:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// var frutat = ["Banane", "Molle", "Portokalle", "Dardhe",];
+
+
+// var emri= "Arbi";
+// console.log(emri.length);
+// console.log("ne kete liste permbane ne total " , frutat.length, "Elemente");
+
+// var vitet = [1990, 1967, 2000];
+
+// function calcAge(birthYear) {
+// return 2025 - birthYear;
+// }
+
+// var age1 = calcAge(vitet[0]);
+// console. log(age1);
+
+// var age2 = calcAge(vitet[1]);
+// console.log(age2);
+
+// var age3 = calcAge(vitet[2]);
+// console.log(age3);
+
+// var frutat = ["Molla", "dardha"];
+// frutat.push("Dredhza");
+// frutat.pop();
+// console.log(frutat);
+
+
+// var words = ["Life", "Is"];
+// words.push("Beautiful");
+// console.log(words);
+// words.shift();
+// console.log(words);
+
+var numrat = [ 5, 17, 59, 73, 89];
+console.log(Math.max(...numrat));
+console.log(Math.min(...numrat));
